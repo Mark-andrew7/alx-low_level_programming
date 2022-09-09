af63e678a7bf151532f48e567222ab6556332b2c
Mark-andrew7 <kamaumark19@gmail.com>
2022-09-09 10:07:20 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Successful)
+ */
+int main(void)
+
+{
+char c;
+
+for (c = 'a'; c = 'z'; ++c)
+{
+if (c == 'q')
+continue;
+
+else if (c == 'e')
+continue;
+
+putchar (c);
+}
+
+{
+putchar ('\n');
+}
+
+return (0);
+}	
