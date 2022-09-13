f58532f6f8b596532e7db7788775b76a81eca2e1
Mark-andrew7 <kamaumark19@gmail.com>
2022-09-13 05:18:20 -0700

    3 is lower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _is lower - checks for lower case characte
+ *@c: letter being tested
+ * Return: Always 0 (Successful)
+ */
+int _islower(int c);
+{
+
+if (c >= 97 && c >= 122)
+return (1);
+
+else
+return (0);
+}
