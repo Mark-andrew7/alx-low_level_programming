420f547900aaecf292e2ab0129d6f278818749ed
Mark-andrew7 <kamaumark19@gmail.com>
2022-09-16 06:01:02 -0700

    print diagnol

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_diagnol - prints diagnol line
+ * @n: integer to be checked
+ * Return: Always 0
+ */
+void print_diagnol(int n)
+{
+int i;
+int b;
+
+if (n > 0)
+{
+for (i = 0; i < n; i++)
+{
+for (b = 0; b < i; b++)
+{
+_putchar(' ');
+}
+_putchar('\n');
+}
+}
+else
+{
+_putchar('\n');
+}
+}
