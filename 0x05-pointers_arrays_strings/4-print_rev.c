927634809bd585647b6db87934bd4c9dffb8b0b4
Mark-andrew7 <kamaumark19@gmail.com>
2022-09-19 07:16:37 -0700

    4 print rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_rev - prints a string in reverse
+ * @s: string to be checked
+ */
+void print_rev(char *s)
+{
+int count = 0;
+
+while (*s != '\0')
+{
+s++;
+count++;
+}
+
+while (count > 0)
+{
+s--;
+_putchar(*s);
+count--;
+}
+_putchar('\n');
+}
