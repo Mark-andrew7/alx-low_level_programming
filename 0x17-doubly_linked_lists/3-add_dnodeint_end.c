e35f5b775cfe3e683b6aaa43fe3a7bb5941ee8f1
Mark-andrew7 <kamaumark19@gmail.com>
2023-01-12 03:55:03 -0800

    3-add_dnodeint_end.c

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -4,7 +4,7 @@
  * dlistint_t list
  * @head: node
  * @n: const int
- * Return: dlistint_t
+ * Return: adress of new element
  */
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
@@ -26,5 +26,10 @@ h = h->next;
 h->next = new;
 }
 else
+{
 *head = new;
+}
+new->prev = h;
+
+return (new);
 }
