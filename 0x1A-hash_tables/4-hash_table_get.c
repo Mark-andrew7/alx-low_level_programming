a2569b34cd866a1b5f002ed8f6663f6d3be40d00
Mark-andrew7 <kamaumark19@gmail.com>
2023-06-16 02:18:36 -0700

    requirements corrections

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -8,7 +8,7 @@
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-hash_node_t *n;
+hash_node_t *node;
 unsigned long int index;
 
 if (ht == NULL || key == NULL || *key == '\0')
@@ -18,9 +18,9 @@ index = key_index((const unsigned char *)key, ht->size);
 if (index >= ht->size)
 return (NULL);
 
-n = ht->array[index];
-while (n && strcmp(node->key, key) != 0)
-n = n->next;
+node = ht->array[index];
+while (node && strcmp(node->key, key) != 0)
+node = node->next;
 
-return ((n == NULL) ? NULL : n->value);
+return ((node == NULL) ? NULL : node->value);
 }
