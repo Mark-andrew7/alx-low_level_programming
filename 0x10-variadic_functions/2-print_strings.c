427050a37ecf2358622d13e78847cb7394016a7f
Mark-andrew7 <kamaumark19@gmail.com>
2023-09-14 10:03:26 -0700

    correction of header files

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,4 +1,6 @@
 #include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
 
 /**
  * print_strings - prints strings
@@ -8,11 +10,14 @@
 void print_strings(const char *separator, const unsigned int n, ...)
 {
 va_list args;
+unsigned int i;
+const char *str;
+
 va_start(args, n);
 
-for (unsigned int i = 0; i < n; i++)
+for (i = 0; i < n; i++)
 {
-const char *str = va_arg(args, const char *);
+str = va_arg(args, const char *);
 
 if (str == NULL)
 printf("(nil)");
