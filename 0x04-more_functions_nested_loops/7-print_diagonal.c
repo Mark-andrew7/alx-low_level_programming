df50c24ced581e8f15d473e4602ecb644ed9b48d
Mark-andrew7 <kamaumark19@gmail.com>
2023-09-18 10:06:28 -0700

    Added descriptive value

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,27 +1,27 @@
 #include "main.h"
 /**
- * print_diagnol - prints diagnol line
+ * print_diagonal - prints diagnol line
  * @n: integer to be checked
  * Return: Always 0
  */
 void print_diagonal(int n)
 {
-int i;
-int b;
+int i, b;
 
-if (n > 0)
+if (n <= 0)
+{
+_putchar('\n');
+}
+else
 {
 for (i = 0; i < n; i++)
 {
 for (b = 0; b < i; b++)
 {
-_putchar(92);
+_putchar(' ');
 }
+_putchar('/');
 _putchar('\n');
 }
-}
-else
-{
-_putchar('\n');
 }
 }
