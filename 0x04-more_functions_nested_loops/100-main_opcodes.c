048a9e1bbdee828f4f2f2360f121f2629a30a0e8
Mark-andrew7 <kamaumark19@gmail.com>
2023-09-18 11:20:41 -0700

    Opcodes

--- 0x04-more_functions_nested_loops/100-main_opcodes.c
+++ 0x04-more_functions_nested_loops/100-main_opcodes.c
@@ -12,7 +12,7 @@ int main(int argc, char *argv[])
 if (argc != 2)
 {
 printf("Error\n");
-return 1;
+return (1);
 }
 
 int num_bytes = atoi(argv[1]);
@@ -20,7 +20,7 @@ int num_bytes = atoi(argv[1]);
 if (num_bytes < 0)
 {
 printf("Error\n");
-return 2; // Negative number of bytes
+return (2);
 }
 
 
@@ -33,5 +33,5 @@ printf(" ");
 }
 printf("\n");
 
-return 0;
+return (0);
 }
