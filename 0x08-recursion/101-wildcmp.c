c430fe5cf6d9440064878c1c76ae4836a02617e6
Mark-andrew7 <kamaumark19@gmail.com>
2023-09-18 12:07:15 -0700

    Inception. Now, before you bother telling me it's impossible...

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdbool.h>
+
+/**
+ * wildcmp - compares two strings with wildcard '*'
+ * @s1: the first string
+ * @s2: the second string with wildcard '*'
+ * Return: 1 if identical, 0 otherwise
+ */
+int wildcmp(char *s1, char *s2)
+{
+if (*s1 == '\0' && *s2 == '\0')
+return (1);
+
+if (*s2 == '*' || *s1 == *s2)
+return wildcmp(s1 + 1, s2 + 1) || wildcmp(s1, s2 + 1);
+
+return (0);
+}
